b00930d75b7efee2313a3d4ab2802782e2508a18
giblets2570 <tomkeohanemurray@gmail.com>
2023-03-23 15:09:51 +0000

    finally got drawing on an image

--- app/AddMaskScreen.js
+++ app/AddMaskScreen.js
@@ -1,89 +1,130 @@
-import React, { useRef, useState } from 'react';
-import { StyleSheet, View, Image, PanResponder, Platform, UIManager, TouchableOpacity } from 'react-native';
-import { Canvas, Image as CanvasImage } from 'react-native-canvas';
+import React, { useRef, useState, useEffect } from 'react';
+import { StyleSheet, View, Image, Text, Dimensions, TouchableOpacity } from 'react-native';
+import Canvas from 'react-native-canvas';
+
+const { width } = Dimensions.get("window")
 
 export default function AddMaskScreen({ navigation, route }) {
-    const [imageUri, setImageUri] = useState(null);
     const canvasRef = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false);
 
-    const handleLayout = () => {
-        canvasRef.current.width = 300;
-        canvasRef.current.height = 300;
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const context = canvas.getContext('2d');
+            context.canvas.width = width;
+            context.canvas.height = width;
+        }
+    }, [canvasRef]);
+
+
+    // Handle starting a new drawing path
+    const handleStartDrawing = ({ nativeEvent }) => {
+        const ctx = canvasRef.current.getContext('2d');
+        const { locationX, locationY } = nativeEvent;
+        ctx.beginPath();
+        ctx.moveTo(locationX, locationY);
+        setIsDrawing(true);
     };
 
-    const panResponder = useRef(
-        PanResponder.create({
-            onStartShouldSetPanResponder: () => true,
-            onPanResponderGrant: (evt) => {
-                const canvas = canvasRef.current;
-                const ctx = canvas.getContext('2d');
-                const { locationX, locationY } = evt.nativeEvent;
-                ctx.beginPath();
-                ctx.arc(locationX, locationY, 25, 0, 2 * Math.PI, true);
-                ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
-                ctx.fill();
-                ctx.strokeStyle = '#FFFFFF';
-                ctx.lineWidth = 2;
-                ctx.stroke();
-                canvas.toDataURL('image/jpeg', 1.0, (data) => setImageUri(data));
-            },
-        })
-    ).current;
-    if (Platform.OS === 'android') {
-        UIManager.setLayoutAnimationEnabledExperimental(true);
-    }
-    // return <View style={styles.screen}>
-    //     <Image source={{ uri: route.params.imageUri }} style={styles.image} />
-    //     <TouchableOpacity
-    //         title="Go to Screen 1"
-    //         onPress={() => {
-    //             navigation.pop()
-    //         }}
-    //     />
-    // </View>
+    // Handle drawing on the canvas
+    const handleDraw = ({ nativeEvent }) => {
+        if (!isDrawing) return;
+        const ctx = canvasRef.current.getContext('2d');
+        const { locationX, locationY } = nativeEvent;
+        ctx.lineTo(locationX, locationY);
+        ctx.stroke();
+    };
 
-    console.log('canvasRef', canvasRef)
-    return <View style={styles.container}>
-        <Image source={{ uri: 'https://picsum.photos/id/1002/300/300' }} style={styles.image} />
-        {/* <Canvas
-            ref={canvasRef}
-            style={styles.canvas}
-            onLayout={() => canvasRef.current.width = 300}
-        >
-            <CanvasImage source={{ uri: imageUri }} style={styles.canvasImage} />
-        </Canvas> */}
-        <View style={styles.overlay} {...panResponder.panHandlers} />
-    </View>
+    // Handle finishing the current drawing path
+    const handleFinishDrawing = () => {
+        console.log('handleFinishDrawing')
+        setIsDrawing(false);
+    };
+
+    // Handle clearing the canvas
+    const handleClearCanvas = () => {
+        const ctx = canvasRef.current.getContext('2d');
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    };
+
+    return (
+        <View style={styles.container}>
+            <Image
+                style={styles.image}
+                source={{ uri: route.params.imageUri }} // Replace with your image source
+                resizeMode="contain"
+                onTouchStart={() => console.log('onTouchStart')}
+                onTouchMove={() => console.log('onTouchMove')}
+                onTouchEnd={() => console.log('onTouchEnd')}
+            />
+
+
+            <View
+                style={styles.canvasContainer}
+                onTouchStart={handleStartDrawing}
+                onTouchMove={handleDraw}
+                onTouchEnd={handleFinishDrawing}
+            >
+
+
+                <Canvas
+                    ref={canvasRef}
+                    style={styles.canvas}
+                    onLayout
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={handleClearCanvas} style={styles.button} >
+                    <Text style={styles.buttonText}>Clear</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={handleClearCanvas} style={styles.button} >
+                    <Text style={styles.buttonText}>Save</Text>
+                </TouchableOpacity>
+            </View>
+        </View >
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#F5FCFF',
     },
     image: {
-        width: 300,
-        height: 300,
-        marginBottom: 20,
+        width: width,
+        height: width
     },
     canvas: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        zIndex: 1,
-    },
-    canvasImage: {
-        width: 300,
-        height: 300,
-    },
-    overlay: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: 300,
-        height: 300,
+        width: width,
+        height: width,
         backgroundColor: 'transparent',
+    },
+    canvasContainer: {
+        position: 'absolute',
+        right: 0,
+        flex: 1,
+        width: width,
+        height: width,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 20,
+        width: width
+    },
+    button: {
+        backgroundColor: '#4F84C4',
+        borderRadius: 20,
+        padding: 10,
+        marginHorizontal: 20,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 18,
     },
 });
