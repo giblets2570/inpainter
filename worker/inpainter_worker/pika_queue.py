14a368ebb659d15597bdff4e0278b0ed239236be
giblets2570 <tomkeohanemurray@gmail.com>
2023-03-26 19:10:26 +0100

    redid the fill algo

--- worker/inpainter_worker/pika_queue.py
+++ worker/inpainter_worker/pika_queue.py
@@ -5,6 +5,7 @@ from inpainter_worker.generator import generate_image
 from tempfile import NamedTemporaryFile
 import numpy as np
 from copy import deepcopy
+from inpainter_worker.shape_filler import fill
 
 params = pika.ConnectionParameters(heartbeat=600,
                                    blocked_connection_timeout=300)
@@ -23,36 +24,7 @@ for method_frame, properties, body in channel.consume('job_request'):
 
     image = Image.open(body['imageFile']).convert(
         "RGB").resize((512, 512)).rotate(-90, expand=True)
-    mask = Image.open(body['maskFile']).resize((128, 128))
-
-    # we need to fill in the mask
-    ma = deepcopy(np.asarray(mask)[:, :, 3])
-    x, y = np.where(ma > 0)
-    coords = list(zip(x, y))
-
-    for i in range(ma.shape[0]):
-        in_shape = False
-        in_line = False
-        was_in_shape = False
-        for j in range(ma.shape[1]):
-            if (i, j) in coords:
-                in_line = True
-                ma[i][j] = 255
-                if in_shape:
-                    was_in_shape = True
-                    in_shape = False
-                continue
-            if in_line:
-                in_line = False
-                if was_in_shape:
-                    in_shape = False
-                    was_in_shape = False
-                else:
-                    in_shape = True
-            if in_shape:
-                ma[i][j] = 255
-
-    mask = Image.fromarray(ma).resize((512, 512))
+    mask = fill(Image.open(body['maskFile']).resize(512, 512))
 
     job_id = body['jobId']
 
